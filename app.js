114f7f582b470c74ff728a896da1d6dfff27be0f
matatsuna <ev50510@meiji.ac.jp>
2018-03-10 14:21:38 +0900

    fix ES5

--- app.js
+++ app.js
@@ -1,52 +1,48 @@
-import express from 'express';
-import fetch from 'node-fetch';
-import cheerio from 'cheerio';
+const express = require('express');
+const fetch = require('node-fetch');
+const cheerio = require('cheerio');
 
-class imageSearch {
-    constructor() {
-        const app = express();
+const app = express();
+const PORT = process.env.PORT || 3000;
 
-        app.get('/', (req, res) => {
-            res.header("Content-Type", "application/json; charset=utf-8");
-            let query = req.query.text;
-            if (query === undefined) {
-                res.send(JSON.stringify([]));
-            }
-            else {
-                this.fetchYahooImg(query)
-                    .then(text => this.parseYahooImg(text))
-                    .then((val) => {
-                        let json = JSON.stringify(val);
-                        res.send(json);
-                    });
-            }
-        })
-
-        app.listen(5070, function () {
-            console.log('Example app listening on port 5070!');
-        })
+app.get('/', (req, res) => {
+    res.header("Content-Type", "application/json; charset=utf-8");
+    let query = req.query.text;
+    if (query === undefined) {
+        res.send(JSON.stringify([]));
     }
-    fetchYahooImg(query) {
-        return new Promise((resolve) => {
-            fetch('https://search.yahoo.co.jp/image/search?p=' + encodeURIComponent(query))
-                .then(e => e.text())
-                .then((text) => {
-                    resolve(text);
-                });
-        });
-    }
-    parseYahooImg(text) {
-        return new Promise((resolve) => {
-            const $ = cheerio.load(text);
-            let urls = [];
-            $('#gridlist a').map((i, element) => {
-                urls.push(element.attribs.href);
+    else {
+        this.fetchYahooImg(query)
+            .then(text => this.parseYahooImg(text))
+            .then((val) => {
+                let json = JSON.stringify(val);
+                res.send(json);
             });
-            resolve(urls);
-        });
-
     }
+})
+
+app.listen(PORT, function () {
+    console.log('Example app listening on port 5070!');
+})
+
+function fetchYahooImg(query) {
+    return new Promise((resolve) => {
+        fetch('https://search.yahoo.co.jp/image/search?p=' + encodeURIComponent(query))
+            .then(e => e.text())
+            .then((text) => {
+                resolve(text);
+            });
+    });
+}
+function parseYahooImg(text) {
+    return new Promise((resolve) => {
+        const $ = cheerio.load(text);
+        let urls = [];
+        $('#gridlist a').map((i, element) => {
+            urls.push(element.attribs.href);
+        });
+        resolve(urls);
+    });
 
 }
 
-const imagesearch = new imageSearch();
